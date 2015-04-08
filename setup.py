c9caa380c64ea25630387a309ae3f4f4f75a4dd2
Mark Fiers <mark.fiers.42@gmail.com>
2015-04-08 14:21:26 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 # one line description
 description = "tools for Mad & Kea"
 
-version = 0.6
+version = 0.6.1
 
 setup(name='toMaKe',
       version=version,
